0afbcd1c23c1d518f8faea1a0ad7aeff9f40c7fd
zhangdaocheng <806186987@qq.com>
2022-10-12 14:03:42 +0800

    变长数组

---
+++ src/week01/lesson02/ResizableArray.java
@@ -0,0 +1,84 @@
+package week01.lesson02;
+
+/**
+ * 变长数组
+ * 新建
+ * get set  （边界检查）
+ * push_back  （在末尾插入）
+ * pop_back （删除末尾元素）
+ */
+public class ResizableArray {
+
+    int size;
+
+    int capcity = 1;
+
+    int[] array;
+
+    public ResizableArray(){
+        array = new int[1];
+    }
+
+    public ResizableArray(int capcity){
+        if(capcity < 1){
+            System.out.println("初始数组容量需大于0");
+        }else {
+            array = new int[capcity];
+        }
+    }
+
+    public int get(int index){
+        try {
+            return array[index];
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("数组越界");
+        }
+        return -1;
+    }
+
+    public void set(int index,int value){
+        try {
+            array[index] = value;
+        } catch (IndexOutOfBoundsException e){
+            System.out.println("数组越界");
+        }
+    }
+
+    public void pushBack(int value){
+        if(size == capcity){
+            capcity = capcity*2;
+            int[] tmp = new int[capcity];
+            for(int i = 0; i < size; i++){
+                tmp[i] = array[i];
+            }
+            array = tmp;
+            array[size] = value;
+            size++;
+        }else{
+            array[size] = value;
+            size++;
+        }
+    }
+
+    public int popBack(){
+        int pop = array[size-1];
+        size--;
+        if(size <= 0.25*capcity){
+            capcity = (int) (0.5*capcity);
+            int[] tmp = new int[capcity];
+            for(int i = 0; i < size; i++){
+                tmp[i] = array[i];
+            }
+            array = tmp;
+        }
+        return pop;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapcity(){
+        return capcity;
+    }
+}
