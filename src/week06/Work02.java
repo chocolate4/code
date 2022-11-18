e235e42291e3faacc11432485f2af8704b7653b3
zhangdaocheng <806186987@qq.com>
2022-11-18 11:02:10 +0800

    week06_work

--- src/week06/Work02.java
+++ src/week06/Work02.java
@@ -10,14 +10,14 @@ import java.util.List;
 public class Work02 {
     public int minimumTotal(List<List<Integer>> triangle) {
         int size = triangle.size();
-        int[][] result = new int[size][size];
-        result[0][0] = triangle.get(0).get(0);
+        int[][] result = new int[size][size]; //创建结果空间
+        result[0][0] = triangle.get(0).get(0); //填入头
         for(int i = 1; i < size; i++){
-            result[i][0] = result[i-1][0] + triangle.get(i).get(0);
+            result[i][0] = result[i-1][0] + triangle.get(i).get(0); //边界特殊值
             for(int j = 1; j < i; j++){
-                result[i][j] = Math.min(result[i-1][j-1],result[i-1][j]) + triangle.get(i).get(j);
+                result[i][j] = Math.min(result[i-1][j-1],result[i-1][j]) + triangle.get(i).get(j); //f[x][y]=min(f[x−1][y−1],f[x−1][y])+c[x][y]
             }
-            result[i][i] = result[i-1][i-1] + triangle.get(i).get(i);
+            result[i][i] = result[i-1][i-1] + triangle.get(i).get(i); //边界特殊值
         }
         return Arrays.stream(result[size-1]).min().getAsInt();
     }
